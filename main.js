a9c319838e1ef34666c6abfc196d5e10b87d2f62
ksubondar <79761397+ksubondar@users.noreply.github.com>
2021-04-14 20:06:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $(".slider").slick(
+    {
+        dots:true,
+        arrows:false,
+        speed:3000,
+        slidesToShow: 3,
+  slidesToScroll: 2,
+  variableWidth: true
+    });
+    
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        $('.header-hover-nav').remove(),
+        $(".burger-menu").css("display", "block"),
+        $(".menuToggle").css("display", "block"),
+        $(".menuToggle").click(function() {
+            $(this).toggleClass("active");
+            $(".hamburger").toggleClass("active");
+            $(".burger-nav ul").slideToggle(300, function(){
+              if($(this).css("display") === "none"){
+                $(this).removeAttr("style"); 
+              }
+            });
+            $(".burger-nav ul").css("display", "flex");
+            $('.burger-nav ul li').on('click', function () {
+              $('.burger-nav ul').hide(100, function(){
+                if ($(this).css('display') === 'none') {
+                  $(this).removeAttr('style');
+                  $(".menuToggle").removeClass("active");
+                  $(".hamburger").toggleClass("active");
+                }
+              });
+            });
+          });
+    } 
+    else;
+});
